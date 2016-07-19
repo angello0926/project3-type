a4bccdf3d1d1b5e0579362760008c9e97663833c
Sherma-n <ysh.sherman@gmail.com>
2016-07-19 13:27:25 +0800

    created conversation model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,15 @@ belongs_to :numerology
 belongs_to :mbti
 belongs_to :horoscope
 
+has_many :conversations, :foreign_key => :sender_id
+
+after_create :create_default_conversation
+
+  # for demo purposes
+
+  def create_default_conversation
+    Conversation.create(sender_id: 1, recipient_id: self.id) unless self.id == 1
+  end
+
+
 end
