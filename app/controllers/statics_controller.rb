b1627634293d22886905e06f37decbba4073c095
Sherma-n <ysh.sherman@gmail.com>
2016-07-19 14:47:51 +0800

    Appended users and chatboxes

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,9 +1,14 @@
 class StaticsController < ApplicationController
 before_action :authenticate_user! , only: [:secret]
 
-def home
+  def home
 
-end
+  end
+
+  def secret
+      @users = User.where.not("id = ?",current_user.id).order("created_at DESC")
+      @conversations = Conversation.involving(current_user).order("created_at DESC")
+  end
 
 
 end
