1d38462df3917588f28906f505ad86f5fed82256
Sherma-n <ysh.sherman@gmail.com>
2016-07-19 15:12:25 +0800

    Added wbesocket(untested)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,6 +16,7 @@
 //= require jquery-deparam/jquery-deparam
 //= require jquery-cookie/jquery.cookie
 //= require pubsub-js/src/pubsub
+//= require private_pub
 //= require chat
 //= require j-toker/dist/jquery.j-toker
 //= require bootstrap/dist/js/bootstrap
