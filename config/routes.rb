e21d9fdf6ff8380b1fa34114f7df2d4c7ea699a4
Angel Lo <angelyukyu@gmail.com>
2016-07-20 16:30:23 +0800

    updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'auth'
-  get 'personalities', to: 'personalities#personality'
+  get '/personalities', to: 'personalities#personality'
+  get '/search', to: 'personalities#search'
+
   get '/secret', to: 'statics#secret'
   root 'statics#home'
 
